f0697866a908c24291142d1a6138b7ffd8229c0e
xspcode <xspcode@gmail.com>
2024-02-23 16:17:54 +0100

    update path service worker

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('codElement/service-worker.js')
         .then((reg) => console.log('Service Worker Registered', reg))
         .catch((err) => console.log('Service Worker Not Registered', err));
 }
